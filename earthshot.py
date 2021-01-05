b07dbb3f7c7ac6b2d9d0b8753e1f8e6b2a0bb921
Jake Creps <crepschrome@gmail.com>
2021-01-05 00:15:15 -0500

    Add files via upload

---
+++ earthshot.py
@@ -0,0 +1,24 @@
+import requests
+import webbrowser
+from selenium import webdriver
+import time
+import csv
+
+zoom = "6970.04703957d"
+
+def earthshot():
+    f = open("coordinates.csv")
+    csv_f = csv.reader(f)
+
+    for coords in csv_f:
+        coords = "".join(coords)
+        url = "https://earth.google.com/web/@" + coords + "," + zoom
+        DRIVER = 'chromedriver'
+        driver = webdriver.Chrome(DRIVER)
+        driver.get(url)
+        time.sleep(6)
+        screenshot = driver.save_screenshot(coords + '.png')
+        driver.quit()
+        print("Screenshot complete!")
+
+earthshot()
